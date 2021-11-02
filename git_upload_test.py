bad413afb8cf07b89dde66b8c0856cd9822c8f32
minho-kim <mincco94@gmail.com>
2021-11-02 19:22:34 +0900

    git_upload_test.py

---
+++ git_upload_test.py
@@ -0,0 +1,49 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.13.0
+#   kernelspec:
+#     display_name: cook
+#     language: python
+#     name: cook
+# ---
+
+import numpy as np
+
+a = np.array([1, 2, 3, 4, 5])
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
